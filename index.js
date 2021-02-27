c5757b64c44c9be1a7be19872ceceeeff2ca46b9
Niels Koster <n.koster@portavita.eu>
2021-02-27 23:46:11 +0100

    Console stuff

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 (_ => {
     const info = document.createElement('div')
     document.body.append(info)
-    
+
+    const style = 'color:white;font-size:22px;text-shadow:1px 1px black'
+
     const showLog = target => {
         console.clear()
-        console.log('Is DevTools open:', target.isOpen)
-        console.log('DevTools orientation:', target.orientation)
+        console.log('oh boy...%cDEVTOOLS!%c(:', style)
+        console.log(`orientation is ${target.orientation}`)
         info.innerHTML = `<pre>devtools is ${target.isOpen ? 'open' : 'closed'}</pre>`
         if (target.orientation)
             info.innerHTML += `<pre>orientation is ${target.orientation}</pre>`
